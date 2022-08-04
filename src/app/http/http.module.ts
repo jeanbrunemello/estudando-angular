9faedb1f3a40a6ec1d7d47ed5add0afb7418be68
f-gabriel-braga <braagaa.inf@hotmail.com>
2022-08-04 16:55:14 -0300

    Aula 4-8-2022 2

---
+++ src/app/http/http.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FilmsComponent } from './films/films.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+@NgModule({
+  declarations: [
+    FilmsComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule
+  ]
+})
+export class HttpModule { }
